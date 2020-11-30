b26a9b9a09a68058e6240feba32c8c36d2d65db9
Ben Langley <ben.langley@deuxfractales.com>
2020-11-29 20:04:37 -0500

    create repost and link to account

--- api/db.js
+++ api/db.js
@@ -1,8 +1,13 @@
 async function db (fastify, options) {
-  
+  const dayjs = require('dayjs')
+  const passGen = require('generate-password')
+
   const MongoClient = require('mongodb').MongoClient;
   const uri = "mongodb+srv://admin:adminben@Cluster0.vb0ok.mongodb.net/otto-users?retryWrites=true&w=majority";
-  const client = new MongoClient(uri, { useNewUrlParser: true });
+  const client = new MongoClient(uri, { 
+    poolSize:10,
+    useNewUrlParser: true
+  });
 
 
   fastify.get('/db', async (request, reply) => {    
@@ -36,6 +41,52 @@ async function db (fastify, options) {
     } finally {
       await client.close();
       reply.send(newUser)
+    }
+  })
+
+  fastify.post('/db/newRepost', async (request,reply) => {
+
+    const repostInfo = {
+      startDate: dayjs().format('DD/MM/YY  hh:mmA'),
+      endDate: dayjs().add(request.body.duration, 'day').format('DD/MM/YY  hh:mmA'),
+      user: request.body.username,
+      hash: passGen.generate({length:15, numbers:true})
+    }
+
+    try {
+      await client.connect();
+      const db0 = client.db('otto-users')
+      const collection0 = db0.collection('customer')
+      const db1 = client.db('Otto-Reposts')
+      const collection1 = db1.collection('current')
+     
+      let query = { username:`${repostInfo.user}` }
+
+      findUser = await collection0.findOne(query)
+      console.log(findUser)
+
+      //addRepost = await collection1.insertOne(repostInfo)
+      
+      //if (addRepost.insertedCount === 1){
+        //console.log(`successfully added repost to db for user ${repostInfo.user} `)
+      //}
+
+      //let update = { $push: {reposts: `${repostInfo.hash}`} }
+      
+      //accountUpdate = await collection0.updateOne(query, update)
+      
+      //if (accountUpdate.modifiedCount === 1) {
+        //console.log(`successfully added repost to ${repostInfo.user}'s account`)
+      //} 
+      // USE THIS FORMAT TO RUN "FIND" QUERY
+      //findAll = await collection0.find(query).toArray(function (err,docs){
+        //console.log(docs)
+      //})
+    } catch (e) {
+      console.log(e)
+    } finally {
+      await client.close()
+      reply.send(`successfully added repost to db, linked to ${repostInfo.user}'s account`) 
     }
   })
   
