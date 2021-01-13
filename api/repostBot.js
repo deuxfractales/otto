501b68f56f917dcf0da07097feddf250e950240b
ben <ben.langle@deuxfractales.com>
2021-01-13 16:05:55 -0500

    using selenium as bot

--- api/repostBot.js
+++ api/repostBot.js
@@ -1,12 +1,24 @@
 async function repostBot (fastify, options) {
-  const puppeteer = require('puppeteer')
+  const {Builder, By, Key, until} = require('selenium-webdriver');
+  
+  const users = [
+    {
+      username:'oototesterone',
+      password: '123ABCabc123!'
+    }
+  ]
 
   fastify.get('/repostBot', async (request, reply) => {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage
-    await page.goto('https://google.com')
-    await broswer.close()
-    return ("opened browser")
+      let driver = await new Builder().forBrowser('firefox').build();
+    try {
+      await driver.get('https://www.kijiji.ca/t-login.html');
+      await driver.findElement(By.id('emailOrNickname')).sendKeys(users[0].username); 
+      await driver.findElement(By.id('password')).sendKeys(users[0].password, Key.RETURN);
+      await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    } finally {
+    await driver.quit();
+    }
+    return("complete")
   })
 }
 
